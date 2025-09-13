2812ff34d2a6ef3d1cd7eaae358326ac349b21e2
gingrspacecadet <jackrawlings2011@gmail.com>
2025-09-13 17:39:55 +0100

    can use hostname instead of just ip

--- main.c
+++ main.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
+#include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <IP>\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <host> [path] [-p port] [-o file]\n", argv[0]);
         return 1;
     }
 
-    const char *ip = argv[1];
+    const char *host = NULL;
+    const char *path = "/";
+    int port = 80;
+    const char *outfile = NULL;
+
+    // parse args
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-p") == 0 && i+1 < argc) {
+            port = atoi(argv[++i]);
+        } else if (strcmp(argv[i], "-o") == 0 && i+1 < argc) {
+            outfile = argv[++i];
+        } else if (!host) {
+            host = argv[i];
+        } else {
+            path = argv[i];
+        }
+    }
+
+    if (!host) {
+        fprintf(stderr, "No host specified\n");
+        return 1;
+    }
+
+    struct hostent *he = gethostbyname(host);
+    if (!he) {
+        fprintf(stderr, "Failed to resolve %s\n", host);
+        return 1;
+    }
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -18,15 +47,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    struct sockaddr_in addr = {0};
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(80); // default HTTP port
-
-    if (inet_pton(AF_INET, ip, &addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        close(fd);
-        return 1;
-    }
+    addr.sin_port = htons(port);
+    memcpy(&addr.sin_addr, he->h_addr_list[0], he->h_length);
 
     if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         perror("connect");
@@ -34,25 +59,36 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("Connected to %s!\n", ip);
+    char request[512];
+    snprintf(request, sizeof(request),
+             "GET %s HTTP/1.0\r\n"
+             "Host: %s\r\n"
+             "\r\n", path, host);
 
-    // send simple HTTP GET
-    const char *request = "GET / HTTP/1.0\r\n\r\n";
     if (write(fd, request, strlen(request)) < 0) {
         perror("write");
         close(fd);
         return 1;
     }
 
-    char buf[512];
-    ssize_t n;
-    while ((n = read(fd, buf, sizeof(buf)-1)) > 0) {
-        buf[n] = '\0';
-        printf("%s", buf);
+    FILE *out = stdout;
+    if (outfile) {
+        out = fopen(outfile, "w");
+        if (!out) {
+            perror("fopen");
+            close(fd);
+            return 1;
+        }
     }
 
+    char buf[1024];
+    ssize_t n;
+    while ((n = read(fd, buf, sizeof(buf)-1)) > 0) {
+        fwrite(buf, 1, n, out);
+    }
     if (n < 0) perror("read");
 
+    if (outfile) fclose(out);
     close(fd);
     return 0;
 }
