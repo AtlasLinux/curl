86ecee1fb54eeb7172c8d96a1b13f2936c254ca6
gingrspacecadet <jackrawlings2011@gmail.com>
2025-10-03 06:46:56 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "curl.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 int main(int argc,char**argv){
     if(argc<2){ fprintf(stderr,"Usage: %s <url> [-o file]\n",argv[0]); return 1; }
